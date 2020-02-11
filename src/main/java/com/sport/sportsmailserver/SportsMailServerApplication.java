55a838a9bc3d9722d0170f3a78d6dbd7cf0f59e7
itning <itning@itning.top>
2020-02-11 17:59:31 +0800

    init

---
+++ src/main/java/com/sport/sportsmailserver/SportsMailServerApplication.java
@@ -0,0 +1,13 @@
+package com.sport.sportsmailserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsMailServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsMailServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sport/sportsmailserver/SportsMailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sport.sportsmailserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsMailServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
